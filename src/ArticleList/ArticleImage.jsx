f9ec19c64c13ae8b2696cb754940d13a2f74c8c2
Ziyao Qiao <qiaoziyao@ZiyaodeMac-mini.local>
2019-11-13 17:23:05 -0800

    add another breakpoint

--- src/ArticleList/ArticleImage.jsx
+++ src/ArticleList/ArticleImage.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './ArticleImage.module.css';
 
 const ArticleImage = (props) => {
     return <img alt={props.title} src={props.url} className={styles.image} />;
-};
-
-ArticleImage.propTypes = {
-    articles: PropTypes.array.isRequired
 };
 
 export default ArticleImage;
